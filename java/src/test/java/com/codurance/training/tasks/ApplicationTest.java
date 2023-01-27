87cde30c33e8669ca4395544cbe7be050c00198f
Lydia Ramos <lydia.ramos@smallworldfs.com>
2023-01-27 12:06:17 +0100

    KATA: Tests

--- java/src/test/java/com/codurance/training/tasks/ApplicationTest.java
+++ java/src/test/java/com/codurance/training/tasks/ApplicationTest.java
@@ -13,6 +13,8 @@ import java.io.PipedOutputStream;
 import java.io.PrintWriter;
 
 import static java.lang.System.lineSeparator;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public final class ApplicationTest {
     public static final String PROMPT = "> ";
@@ -51,51 +53,65 @@ public final class ApplicationTest {
         throw new IllegalStateException("The application is still running.");
     }
 
-    @Test(timeout = 1000) public void
+    @Test(timeout = 10000000) public void
     it_works() throws IOException {
         execute("show");
 
         execute("add project secrets");
-        execute("add task secrets Eat more donuts.");
-        execute("add task secrets Destroy all humans.");
+        execute("add task secrets id1 Eat more donuts.");
+        execute("add task secrets id2 Destroy all humans.");
 
         execute("show");
         readLines(
             "secrets",
-            "    [ ] 1: Eat more donuts.",
-            "    [ ] 2: Destroy all humans.",
+            "    [ ] id1: Eat more donuts.",
+            "    [ ] id2: Destroy all humans.",
             ""
         );
 
         execute("add project training");
-        execute("add task training Four Elements of Simple Design");
-        execute("add task training SOLID");
-        execute("add task training Coupling and Cohesion");
-        execute("add task training Primitive Obsession");
-        execute("add task training Outside-In TDD");
-        execute("add task training Interaction-Driven Design");
+        execute("add task training id3 Four Elements of Simple Design");
+        execute("add task training id4 SOLID");
+        execute("add task training id5 Coupling and Cohesion");
+        execute("add task training id6 Primitive Obsession");
+        execute("add task training id7 Outside-In TDD");
+        execute("add task training id8 Interaction-Driven Design");
 
-        execute("check 1");
-        execute("check 3");
-        execute("check 5");
-        execute("check 6");
+        execute("check id1");
+        execute("check id3");
+        execute("check id5");
+        execute("check id6");
 
         execute("show");
         readLines(
                 "secrets",
-                "    [x] 1: Eat more donuts.",
-                "    [ ] 2: Destroy all humans.",
+                "    [x] id1: Eat more donuts.",
+                "    [ ] id2: Destroy all humans.",
                 "",
                 "training",
-                "    [x] 3: Four Elements of Simple Design",
-                "    [ ] 4: SOLID",
-                "    [x] 5: Coupling and Cohesion",
-                "    [x] 6: Primitive Obsession",
-                "    [ ] 7: Outside-In TDD",
-                "    [ ] 8: Interaction-Driven Design",
+                "    [x] id3: Four Elements of Simple Design",
+                "    [ ] id4: SOLID",
+                "    [x] id5: Coupling and Cohesion",
+                "    [x] id6: Primitive Obsession",
+                "    [ ] id7: Outside-In TDD",
+                "    [ ] id8: Interaction-Driven Design",
                 ""
         );
 
+        execute("deadline id2 27/01/2023");
+        execute("deadline id4 27/01/2023");
+        execute("deadline id6 26/01/2023");
+        execute("today");
+        readLines(
+                "secrets",
+                "    [ ] id2: Destroy all humans.",
+                "",
+                "training",
+                "    [ ] id4: SOLID",
+                ""
+        );
+        execute("add task training id& wrongTaskOnPurpose");
+        readLines("Could not create the task because the id contains special characters");
         execute("quit");
     }
 
@@ -108,7 +124,7 @@ public final class ApplicationTest {
         int length = expectedOutput.length();
         char[] buffer = new char[length];
         outReader.read(buffer, 0, length);
-        //assertThat(String.valueOf(buffer), is(expectedOutput));
+        assertThat(String.valueOf(buffer), is(expectedOutput));
     }
 
     private void readLines(String... expectedOutput) throws IOException {
